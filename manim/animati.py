d45d50838dd1080ec4a0df190c5b751978ff25e6
Designedby-Radol <raulsantiagoramirez1@gmail.com>
2024-06-02 15:24:29 -0500

    Commit desde linux

--- manim/animati.py
+++ manim/animati.py
@@ -1,22 +1,5 @@
 from manim import *
 
-# generar fibonacci
-def fibonacci_gen(num):
-    fn = [0, 1]
-    while fn[-1] <= num:
-        fn.append(fn[-1] + fn[-2])
-    if fn[-1] > num:
-        fn.pop()
-    # for valor in fn:
-    #     print(valor)
-    return fn
-
-# Ejemplo de uso:
-numlimit = int(input("ingrese el numero al que quiera ir hasta foibonacci: "))  # Puedes cambiar este valor según desees
-fib = fibonacci_gen(numlimit) #
-print(f"Secuencia de Fibonacci hasta el número {numlimit}:")
-print(fib)
-
 class TresCuadrados(Scene):
     def construct(self):
         # Crear los primeros dos cuadrados con tamaño 1
